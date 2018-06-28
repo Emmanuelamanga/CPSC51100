a5162992a25a0b1e1ce9ce5bf8d79e5d74b4c7be
Don Cruver <donald.cruver@connexta.com>
2018-06-27 22:21:04 -0700

    completed bar chart

--- vispums/vispums.py
+++ vispums/vispums.py
@@ -7,28 +7,43 @@
 # Assignment Name: Programming Assignment 6 – Visualizing ACS PUMS Data
 
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib
+#import matplotlib
 
 def household_language_plot(fig, hhl):
     lang_lookup = ['English only',
                     'Spanish',
                     'Other Indo-European languages',
                     'Asian and Pacific Island languages',
-                    'Other language']  
-             
-    ax1 = fig.add_subplot(2, 2, 1)
-    pie = ax1.pie(hhl, startangle=-117)
-    ax1.legend(lang_lookup, loc='upper left', bbox_to_anchor=(-0.3, 0.93), fontsize='small')
+                    'Other language']
+
+    ax = fig.add_subplot(2, 2, 1)
+    pie = ax.pie(hhl, startangle=-117)
+    ax.legend(lang_lookup, loc='upper left', 
+              bbox_to_anchor=(-0.3, 0.93), 
+              fontsize='small')
     plt.title(y = 0.92, s='Household Languages', fontsize='small')
 
 def household_income_plot(fig, hincp):
-    ax1 = fig.add_subplot(2, 2, 2)
-    hist = ax1.hist2d(x=pd.cut(hincp.HINCP, np.logspace(start=1, stop=7)),
-                      y=hincp.HINCP, bins='auto') 
+    ax = fig.add_subplot(2, 2, 2)
+    x_range = np.logspace(0, 7, base=10)
+    hist = ax.hist2d(x=hincp.values,
+                     y=hincp.index)
+    
+def household_vehicle_plot(fig, veh):
+    ax = fig.add_subplot(2, 2, 3)
+    bar = ax.bar(veh.index, 
+                 veh.WGTP / 1000, 
+                 align='center', 
+                 color='red')
+    plt.ylabel('Thousands of Households')
+    plt.xlabel('# of Vehicles')
+    plt.title(y = 1.0, s='Vehicles Available in Households', fontsize='small')
 
 fig = plt.figure(figsize=(12, 12))
 df = pd.read_csv("ss13hil.csv")
 household_language_plot(fig, df.groupby('HHL').aggregate({'SERIALNO' : 'count'}))
-household_income_plot(fig, df)
-print(df)
+household_income_plot(fig, df.HINCP[df.HINCP.notna()])
+household_vehicle_plot(fig, df.groupby('VEH').aggregate({'WGTP':'sum'}))
+plt.show()
